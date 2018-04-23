6a0bd54fe1738d90e1c38e32b623bb22cc391b96
TAY40401639 <40401639@napier.ac.uk>
2018-04-23 14:47:10 +0630

    template inherit

--- condition.py
+++ condition.py
@@ -11,5 +11,18 @@ def users():
 	names=['Thomas','simon','lee','jamie','sylvester']
 	return render_template('loop.html',names=names)
 
+
+@app.route('/inherits/')
+def inherits():
+	return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+	return render_template('in1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+	return render_template('in2.html')
+
 if __name__=="__main__":
 	app.run(debug=True)
