69e47cd18f87d7010c33c0f6ac03c87ce0e50d03
TAY40401639 <38060877+TAY40401639@users.noreply.github.com>
2018-04-25 11:18:59 +0630

    add server

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('condition.html',name=name)
+
+@app.route('/users/')
+def users():
+	names=['Thomas','simon','lee','jamie','sylvester']
+	return render_template('loop.html',names=names)
+
+
+@app.route('/inherits/')
+def inherits():
+	return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+	return render_template('in1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+	return render_template('in2.html')
+
+
+
+@app.route('/')
+def root():
+	return render_template('boo.html'),200
+
+
+if __name__=="__main__":
+	app.run(debug=True)
